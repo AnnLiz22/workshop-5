18e90f433786671fbb5588e69a45db5b744c8a0f
AnnLiz22 <bielakam@gmail.com>
2024-05-24 16:29:09 +0200

    BookService interface and MockBookService

---
+++ src/main/java/pl/coderslab/BookService.java
@@ -0,0 +1,9 @@
+package pl.coderslab;
+
+import java.util.List;
+
+public interface BookService {
+
+    List<Book> getBooks();
+}
+
--- src/main/java/pl/coderslab/MockBookService.java
+++ src/main/java/pl/coderslab/MockBookService.java
@@ -1,6 +1,27 @@
 package pl.coderslab;
 
-public class MockBookService {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+@Getter
+@Setter
+public class MockBookService implements BookService{
+
+    private List<Book> books;
+    public MockBookService() {
+        books = new ArrayList<>();
+        books.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce	Eckel", "Helion", "programming"));
+        books.add(new Book(2L, "9788324627738", "Rusz	glowa	Java.", "Sierra	Kathy,	Bates	Bert", "Helion",
+                "programming"));
+        books.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
+                "programming"));
+    }
+
 
     public void addBook(){
 
@@ -13,8 +34,6 @@ public class MockBookService {
     public void deleteBook(){
 
     }
-
-    public void getAllBooks(){}
 
     public void getBook(){}
 }
