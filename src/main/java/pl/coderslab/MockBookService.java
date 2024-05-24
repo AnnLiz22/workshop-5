671028909e6bf32c00507e438e1bf6e8819180f6
AnnLiz22 <bielakam@gmail.com>
2024-05-24 16:51:42 +0200

    refactor

--- src/main/java/pl/coderslab/MockBookService.java
+++ src/main/java/pl/coderslab/MockBookService.java
@@ -2,7 +2,6 @@ package pl.coderslab;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
