7d9b686bd69d8a36c9fbc218332c0a3e6f814698
AnnLiz22 <bielakam@gmail.com>
2024-09-09 21:59:02 +0200

    added book service test

---
+++ src/test/java/pl/coderslab/BookServiceTest.java
@@ -0,0 +1,26 @@
+package pl.coderslab;
+
+import org.junit.jupiter.api.Test;
+
+class BookServiceTest {
+
+    @Test
+    void getBooks() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+}
